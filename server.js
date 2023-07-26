c2b6cf777e6947280fd9760d755bc83dae6bd294
ArtemBielyi <kolhozreshaet@gmail.com>
2023-07-26 18:54:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = require("./app");
 const DB_HOST =
   "mongodb+srv://ArtemBielyi:25091992@cluster1.rq6z8r3.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
+mongoose.set("strictQuery", true);
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
